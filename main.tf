cee65feb3898a7d0eda7c12b59ecc3ffa1d2ddd9
Chris <chris.mullin@nttdata.com>
2020-10-26 07:11:15 -0700

    Initial module commit

---
+++ main.tf
@@ -0,0 +1,92 @@
+resource "random_string" "prefix" {
+  length  = 8
+  special = false
+  lower   = true
+  upper   = false
+  number  = true
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.region
+  # TODO: Add Tag support
+  #tags     = local.common_tags
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  addon_profile {
+    kube_dashboard {
+      enabled = true
+    }
+  }
+
+  # TODO: add tag support
+  #tags = local.common_tags
+
+}
+
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+
+  # TODO: add tag support
+  tags = local.common_tags
+
+}
+
+
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = var.cosmosdb_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = false
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+# General module variables
+variable "rg_name" {
+    type = string
+    description = "Provide a name to give to the resource group"
+}
+
+variable "location" {
+    type = string
+    description = "The region/location within Azure that these resources should be stood up."
+}
+
+
+# AKS Variables
+variable "aks_cluster_name" {
+    type = string
+    description = "The name used for the AKS cluster"
+}
+
+variable "dns_prefix" {
+    type = string
+    description = "The DNS prefix used within the AKS definition"
+}
+
+variable "client_id" {
+    type = string
+    description = "The id of the principle client"
+}
+
+variable "client_secret" {
+    type = string
+    description = "The secret/password for this client_id account"
+}
+
+
+# ACR variables
+variable acr_name {
+    type = string
+    description = "The name used for the Azure Container Registry (ACR)"
+}
+
+
+# Cosomos DB variables
+variable cosmosdb_name {
+    type = string
+    description = "The name of the CosmosDB"
+}
+
+# TODO: will need to add more cosmos variables to support a failover environment and allow for multi database.
+# TODO: currently only useing a single database in a single region.
