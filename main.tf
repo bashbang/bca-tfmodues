cdc25ac31e0c87b4402bd4c6b470321377ca6a92
Chris <chris.mullin@nttdata.com>
2020-11-02 10:45:42 -0800

    re-added cosmos with aggPipeline support

--- main.tf
+++ main.tf
@@ -71,31 +71,35 @@ resource "azurerm_role_assignment" "acrpull_role" {
   skip_service_principal_aad_check = true
 }
 
-# resource "azurerm_cosmosdb_account" "db" {
-#   name                = var.cosmosdb_name
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   offer_type          = "Standard"
-#   kind                = "MongoDB"
+resource "azurerm_cosmosdb_account" "db" {
+  name                = var.cosmosdb_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
 
-#   enable_automatic_failover = false
+  enable_automatic_failover = false
 
-#   capabilities {
-#     name = "mongoEnableDocLevelTTL"
-#   }
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
 
-#   capabilities {
-#     name = "MongoDBv3.4"
-#   }
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
 
-#   consistency_policy {
-#     consistency_level       = "BoundedStaleness"
-#     max_interval_in_seconds = 10
-#     max_staleness_prefix    = 200
-#   }
+  capabilities {
+    name = "MongoDBv3.4"
+  }
 
-#   geo_location {
-#     location          = azurerm_resource_group.rg.location
-#     failover_priority = 0
-#   }
-# }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
