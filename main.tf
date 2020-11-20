afc45e2b807c6d5adce3da66077ef8d3609dc4b5
Chris <chris@bashbang.com>
2020-11-20 08:41:12 -0800

    syntax fixes

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "azurerm_cosmosdb_account" "db" {
 }
 
 resource "azurerm_key_vault" "akv" {
-  name                        = "var.avk_name${random_string.prefix.id}"
+  name                        = "${var.avk_name}-${random_string.prefix.id}"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
